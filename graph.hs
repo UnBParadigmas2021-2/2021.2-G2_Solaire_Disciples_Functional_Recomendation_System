2e5a7e1b8951beca034b4ff7a4ed391a39d4e835
Filipe Machado <filipe__machado@hotmail.com>
2022-02-13 17:29:42 -0300

    adiciona recomendaçao de amizades

--- graph.hs
+++ graph.hs
@@ -122,15 +122,13 @@ getCommonFriends g a b = getFriends g a `intersect` getFriends g b
 concatList:: [Int] -> [Int] -> [Int]
 concatList x y = nub (x ++ y)
 
--- Conta quantos amigos o no 'a' tem em comum com o no 'b'
-
 -- TODO -> Recommend by influence -> substituir o length (que considera cada amigo como 1)
 -- por um sistema que penaliza o score baseado em quantos amigos cada item de getCommonFriends tem (n). -> 1/n1 + 1/n2 + 1/n3 ...
 -- exemplo: https://courses.cs.washington.edu/courses/cse140/13wi/homework/hw4/homework4.html problem 3
+
+-- Conta quantos amigos o no 'a' tem em comum com o no 'b'
 countCommonFriends :: Eq a => Graph a -> a -> a -> Int
 countCommonFriends g a b = length(getCommonFriends g a b)
-
-
 
 -- Transforma toda a lista de no em (score, id)
 addAllPeopleScore :: Eq b => Graph b -> b -> [b] -> [(Int, b)]
@@ -143,3 +141,7 @@ addPeopleScore g a b = (countCommonFriends g a b, b)
 -- Ordena nó por quantidade total de amigos em comum
 sortByCommonFriends :: Ord b1 => [(b1, b2)] -> [(b1, b2)]
 sortByCommonFriends a = reverse (sortOn fst a)
+
+-- Retorna recomendação de amizades
+generateRecomendations :: Eq a => Graph a -> a -> [(Int, a)]
+generateRecomendations g a = sortByCommonFriends(addAllPeopleScore g a (foaf g a))
