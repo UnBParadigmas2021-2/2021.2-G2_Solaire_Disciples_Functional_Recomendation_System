44d9bb4368d272a7c4a7108e15db3cde721f401a
Filipe Machado <filipe__machado@hotmail.com>
2022-02-13 18:18:46 -0300

    add modulo para relacionar id com nome de pessoa

---
+++ Identification.hs
@@ -0,0 +1,33 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ViewPatterns #-}
+
+module Identification where
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
+
+people :: Map Integer String 
+people = Map.fromList [(1, "Joao Pe de Feijao")
+                      ,(2, "Maria Silva Sousa")
+                      ,(3, "Pedro Marcos")
+                      ,(4, "Felipe Homogeneo")
+                      ,(5, "Giuseppe Cadura")
+                      ,(6, "Guilherme Coelho")
+                      ,(7, "Sheila Behringer")
+                      ,(8, "Marcela Santos")
+                      ,(9, "John Lennon")
+                      ,(10, "Jade Picon")
+                      ,(11, "Ednaldo Pereira")
+                      ,(12, "Juliana Bonde")
+                      ,(13, "Fabio de Melo")
+                      ,(14, "Luan Santana")
+                      ,(15, "Gustavo Lima")
+                      ,(16, "Marilia Mendonca")
+                      ,(17, "Linus Tech Tips")]
+
+findPeopleById :: Integer -> String
+findPeopleById id = case Map.lookup id people of
+                 Nothing  -> "Kage Bushin no Jutsu No. " ++ show id
+                 Just name -> name
