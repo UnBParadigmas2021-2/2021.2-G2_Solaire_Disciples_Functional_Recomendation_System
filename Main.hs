ee421521799645e5a4b18bb3761e8ba0b529a6c4
Cainã <caina20000@gmail.com>
2022-02-14 10:43:51 -0300

    adiciona renderer de html

--- Main.hs
+++ Main.hs
@@ -1,19 +1,32 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 import Recomendation
 import RecomendationInfluence
 import Graph
 import Identification
 import NetworkExamples
-import Web.Scotty ( get, html, scotty )
-import qualified Data.Text as T
-import Data.Monoid (mconcat)
-import Data.Text.Lazy ( pack )
+import Web.Scotty
+import Text.Hamlet (shamlet)
+import Text.Blaze.Html.Renderer.String (renderHtml)
+import Text.Blaze.Renderer.Utf8 (renderMarkup)
 
 
+render = raw . renderMarkup
 
-main = scotty 3000 $
-  get "/" $
-  html (pack "<h1>Ola, " <> pack (concat (getAllRecomendationsString [(2,1),(3,4)])))
+data Person = Person
+    { name :: String
+    , _id  :: Int
+    }
+
+
+main = scotty 3000 $ do
+  get "/" $ do
+    render [shamlet|
+      <p>Meu nome: #{name person}<p>
+      <p>Meu Id:#{show $ _id person}<p>
+    |]
+  where
+    person = Person (findPeopleById 1) (1)
